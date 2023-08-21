5c773f538ea86e66de55fa8a856a0568cc0f94ab
quentincloudsnow <quentin.carton@servicenow.com>
2023-08-21 13:31:55 -0700

    Create node.js

---
+++ node.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const data = require('./data.json'); // Assuming data.json is in the same directory
+
+app.get('/checkUser', (req, res) => {
+  const { username } = req.query;
+  const userExists = data.some(user => user.username === username);
+  if (userExists) {
+    res.status(200).json({ message: 'User exists' });
+  } else {
+    res.status(404).json({ message: 'User does not exist' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
