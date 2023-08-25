e9717ccef21b75f215e13f38331bd2e02d006b41
quentin.carton@servicenow.com <quentin.carton@servicenow.com>
2023-08-25 08:45:41 -0700

    Added additional field

--- node.js
+++ node.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
-const data = require('./data.json'); // Assuming data.json is in the same directory
-//ADDED CORS CONFIG
+const data = require('./data.json');
 
 // CORS middleware
 app.use((req, res, next) => {
@@ -14,25 +13,40 @@ app.use((req, res, next) => {
     }
 });
 
-// ... rest of your app's routes and logic
-//Quentin to add error handling
-
 app.get('/checkUser', (req, res) => {
   const { firstname, lastname, dateofbirth } = req.query;
 
-  const userExists = data.some(user =>
+  const user = data.find(user =>
     user.firstname === firstname &&
     user.lastname === lastname &&
     user.dateofbirth === dateofbirth
   );
-const response = {
-    code: userExists ? 0 : 1,
-    message: userExists ? 'User exists' : 'User does not exist'
-  };
 
-  res.status(200).json(response);
+  if (user) {
+    const response = {
+      code: 0,
+      message: 'User exists',
+      user: {
+        guest_title: user.guest_title,
+        phone: user.phone,
+        host_name: user.host_name,
+        host_id_number: user.host_id_number,
+        host_email: user.host_email,
+        guest_email: user.guest_email,
+        building_location: user.building_location,
+        access_expiration: user.access_expiration
+      }
+    };
+    res.status(200).json(response);
+  } else {
+    const response = {
+      code: 1,
+      message: 'User does not exist'
+    };
+    res.status(200).json(response);
+  }
 });
 
 app.listen(3000, () => {
-          console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000');
 });
